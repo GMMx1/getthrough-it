15cd51570d9caa8e4ed22159bf056daa82e50439
Juan Gabriel <juangabriel@pixeloak.com>
2017-07-20 13:46:10 -0500

    Added npm scripts for deployment

--- config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = process.env.PORT || 8000
-export const SECRET_KEY = process.env.SECRET_KEY
-export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
-export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
-export const GITHUB_CALLBACK_URL = process.env.GITHUB_CALLBACK_URL
--- middlewares/decodeId.js
+++
@@ -1,7 +0,0 @@
-import Hash from '../utils/Hash'
-
-const decodeId = (req, res, next) => {
-  if (req.params.id) {
-    req.params.id = 
-  }
-}
