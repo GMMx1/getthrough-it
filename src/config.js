00f952e02eae8d025e712a3e08d4b2c5f2227d91
Juan Gabriel <juangabriel@pixeloak.com>
2017-07-24 19:07:00 -0500

    Add different environment for github oauth

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const ENV = process.env.NODE_ENV || 'development'
 export const isProd = ENV === 'production'
 export const PORT = process.env.PORT || 8000
 export const SECRET_KEY = process.env.SECRET_KEY
-export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
-export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
-export const GITHUB_CALLBACK_URL = isProd ? process.env.GITHUB_CALLBACK_URL : 'http://localhost:8000/v1/auth/github/callback'
+export const GITHUB_CLIENT_ID = process.env.[isProd ? 'GITHUB_CLIENT_ID_PROD' : 'GITHUB_CLIENT_ID']
+export const GITHUB_CLIENT_SECRET = process.env.[isProd ? 'GITHUB_CLIENT_SECRET_PROD' : 'GITHUB_CLIENT_SECRET']
+export const GITHUB_CALLBACK_URL = process.env.[isProd ? 'GITHUB_CALLBACK_URL_PROD' : 'GITHUB_CALLBACK_URL']
