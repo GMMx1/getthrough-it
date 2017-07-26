d530ad7720e692592d2f64a9c7c65019dd044bda
Juan Gabriel <juangabriel@pixeloak.com>
2017-07-25 21:21:45 -0500

    Fixed deployment issues

--- src/middlewares/enableCors.js
+++ src/middlewares/enableCors.js
@@ -1,5 +1,15 @@
+import { isProd } from '../config'
+
 const enableCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  const allowedOrigins = ["https://www.getthrough.it", "https://getthrough.it"]
+  const origin = req.headers.origin
+
+  if (isProd && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin)
+  } else {
+    console.log(origin)
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+  }
   res.header("Access-Control-Allow-Credentials", true)
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST")
